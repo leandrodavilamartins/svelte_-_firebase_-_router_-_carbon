f536dd0bcf4607e10835b883e29d209814acf38d
leandrodavilamartins <39203114+leandrodavilamartins@users.noreply.github.com>
2023-02-16 20:44:46 -0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -3,13 +3,7 @@ import { initializeApp } from '../node_modules/firebase/app';
 import { getFirestore } from '../node_modules/firebase/firestore';
 
 const firebaseConfig={
-	  	apiKey: "AIzaSyAsWNihnM84o7kmb7HqlWuV_GhpLMt7Wak",
-  		authDomain: "serene-circlet-329302.firebaseapp.com",
-  		projectId: "serene-circlet-329302",
-  		storageBucket: "serene-circlet-329302.appspot.com",
-  		messagingSenderId: "1066497611107",
-  		appId: "1:1066497611107:web:b997ede6c4954ef2df2f56",
-  		measurementId: "G-SGWTMRXT59"
+	// config data 
 }
 
 const firebase = initializeApp(firebaseConfig);
